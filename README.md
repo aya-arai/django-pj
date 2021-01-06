5ab2d1ab712f086c43de0f99456e79f8787ad346
aya_arai <aya_arai@change-jp.com>
2021-01-06 11:03:36 +0900

    first Commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # django-pj
-Pythonフレームワークのdjangoの環境
+
+# 起動コマンド
+cd 開発コーディング/django-pj/
+source django_venv/bin/activate
+cd sakusaku_blog
+python3 manage.py runserver
---
+++ django_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/aya_arai/開発コーディング一覧/EmviList/django-pj/django_venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
